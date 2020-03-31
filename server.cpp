eac9e49b52ba091866e38745293b05b89b4ade90
New91 <niu91@qq.com>
2020-03-31 22:32:41 +0800

    server 使用select

--- server.cpp
+++ server.cpp
@@ -5,6 +5,13 @@
 
 int main(int argc, char **argv)
 {
+    int i,maxi,maxfd,listenfd,connfd,sockfd;
+    int nready,client[FD_SETSIZE];
+    ssize_t n;
+    fd_set rset;
+    fd_set allset;
+    char buf[MAX_BUFF_LEN];
+    socklen_t clilen;
 
     // 1. 服务器创建一个socket
     //  AF_INET             IPv4 Internet protocols          ip(7)
@@ -40,36 +47,60 @@ int main(int argc, char **argv)
 
     struct sockaddr_in clt_addr;
     
-    time_t ticks;
-    int conn_fd  = 0;
-    pid_t child_pid = 0;
-    char buff[MAX_BUFF_LEN] = {0};//缓冲区
-    Signal(SIGCHLD, sig_child); // 处理僵死进程
+    maxfd = serv_sock;
+    maxi = -1;
+    for(i = 0; i<FD_SETSIZE;i++)
+        client[i] = -1;
+    FD_ZERO(&allset);
+    FD_SET(serv_sock, &allset);
+
     while(1)
     {
-        int len = sizeof(clt_addr);
-        conn_fd = accept(serv_sock, (struct sockaddr *)&clt_addr, (socklen_t *)&len);
-        if(conn_fd < 0){
-            if(errno == EINTR)
-                continue;
-            else
-            {
-                fprintf(stderr, "accept error\n");
-            }
-            
-        }
-        printf("Info: connect from %s, port %d\n", 
-            inet_ntop(AF_INET, &clt_addr.sin_addr, buff, sizeof(buff)),
-            ntohs(clt_addr.sin_port));
+        rset = allset;
+        nready = select(maxfd+1, &rset, NULL, NULL, NULL);
 
-        if ((child_pid = fork()) == 0)// 子进程来处理连接的业务逻辑
+        if(FD_ISSET(serv_sock, &rset))//新的连接来了
         {
-            close(serv_sock);
-            do_it(conn_fd);
-            exit(0);
+            clilen = sizeof(clt_addr);
+            connfd = accept(serv_sock, (struct sockaddr *)&clt_addr, (socklen_t *)&clilen);
+
+            for(i = 0; i<FD_SETSIZE;i++)
+            {
+                if(client[i]<0){
+                    client[i] = connfd;
+                    break;
+                }
+                if(i==FD_SETSIZE)
+                    fprintf(stderr, "too many clients");
+            }
+
+            FD_SET(connfd, &allset);
+            if(connfd > maxfd)
+                maxfd = connfd;
+            if(i>maxi)
+                maxi = i;
+            if(--nready <= 0)
+                continue;
         }
 
-        close(conn_fd);
+
+        for(int i=0; i<=maxi; i++)
+        {
+            if((sockfd = client[i]) < 0)
+                continue;
+            if(FD_ISSET(sockfd, &rset)){
+                if((n= read(sockfd, buf, MAX_BUFF_LEN)) == 0){
+                    close(sockfd);
+                    FD_CLR(sockfd,&allset);
+                    client[i] = -1;
+                }else{
+                    write(sockfd, buf,n);
+                }
+
+                if(--nready <= 0)
+                    break;
+            }
+        }
     }
 
     printf("hello world!\n");
